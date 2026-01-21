ba6ebefb9f374fa3e625553e4a8419cfb61a9bfc
Noor <noor.kammoun99@gmail.com>
2026-01-21 12:34:41 +0100

    variables names changed from abrevation to full name for better reading comprehension

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const myForm = document.getElementById('astronomy-form');
-const msgError = document.getElementById('message-error');
-const msgSuccess = document.getElementById('message-success');
+const astronomyForm = document.getElementById('astronomy-form');
+const errorMessageDisplay = document.getElementById('message-error');
+const successMessageDisplay = document.getElementById('message-success');
 
-myForm.addEventListener('submit', (event) => {
+astronomyForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const latValue = parseFloat(document.getElementById('lat').value);
-    const lonValue = parseFloat(document.getElementById('lon').value);
-    const altValue = parseFloat(document.getElementById('alt').value);
+    const latitudeValue = parseFloat(document.getElementById('latitude').value);
+    const longitudeValue = parseFloat(document.getElementById('longitude').value);
+    const altitudeValue = parseFloat(document.getElementById('altitude').value);
     const dateValue = document.getElementById('date').value;
 
-    msgError.innerText = "";
-    msgSuccess.innerText = "";
+    errorMessageDisplay.innerText = "";
+    successMessageDisplay.innerText = "";
 
 
-    if (latValue < -90 || latValue > 90) {
-        msgError.innerText = "Erreur : La latitude doit être entre -90 et 90.";
+    if (latitudeValue < -90 || latitudeValue > 90) {
+        errorMessageDisplay.innerText = "Erreur : La latitude doit être entre -90 et 90.";
         return;
     }
 
-    if (lonValue < -180 || lonValue > 180) {
-        msgError.innerText = "Erreur : La longitude doit être entre -180 et 180.";
+    if (longitudeValue < -180 || longitudeValue > 180) {
+        errorMessageDisplay.innerText = "Erreur : La longitude doit être entre -180 et 180.";
         return;
     }
 
-    const lat = latValue;
-    const lon = lonValue;
-    const alt = altValue;
+    const latitude = latitudeValue;
+    const longitude = longitudeValue;
+    const altitude = altitudeValue;
     const date = dateValue;
 
 
-    console.log("Données enregistrées :", { lat, lon, alt, date });
-    msgSuccess.innerText = "Succès ! Les coordonnées sont valides.";
+    console.log("Données enregistrées :", { latitude, longitude, altitude, date });
+    successMessageDisplay.innerText = "Succès ! Les coordonnées sont valides.";
 });
