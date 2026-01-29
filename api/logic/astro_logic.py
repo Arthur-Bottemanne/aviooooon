4000fa4dff67a472701c8fe285ff0df728c5d4de
Anthony Simond <pv22nbo@eduvaud.ch>
2026-01-29 08:59:37 +0100

    chore: change name file

--- api/logic/astro_logic.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def calculate_will_intersect(future_azimuth,moon_azimuth,future_elevation,moon_elevation,threshold=0.35):
-
-    distance = math.sqrt((future_azimuth - moon_azimuth)**2 + (future_elevation - moon_elevation)**2)
-
-    return distance <= threshold
--- api/logic/plane_moon_overlap_prediction.py
+++ api/logic/plane_moon_overlap_prediction.py
@@ -14,3 +14,10 @@ def predict_future_position(latitude, longitude, altitude,velocity,heading,verti
     future_longitude = longitude + math.degrees((distance_metre * math.sin(heading_radians)) / (earth_radius * math.cos(math.radians(latitude))))
 
     return future_latitude, future_longitude, future_altitude
+
+
+def calculate_will_intersect(future_azimuth,moon_azimuth,future_elevation,moon_elevation,threshold=0.35):
+
+    distance = math.sqrt((future_azimuth - moon_azimuth)**2 + (future_elevation - moon_elevation)**2)
+
+    return distance <= threshold
